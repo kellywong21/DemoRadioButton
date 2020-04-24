d88b66878c1530da31b3dd74c98dff6dbe0d29bd
18004885 <18004885@myrp.edu.sg>
2020-04-24 11:02:22 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoradiobutton/MainActivity.java
@@ -0,0 +1,21 @@
+package sg.edu.rp.c346.demoradiobutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
